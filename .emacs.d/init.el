0e79d93c5ecf0759fe3dbe4ccfde583c754cb020
James Craig Burley <james-github@burleyarch.com>
2017-11-03 13:20:16 -0400

    Fancier "git push" support

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -17,11 +17,20 @@
 
 (global-set-key (kbd "C-c w") 'compare-windows)
 
-(global-set-key (kbd "C-x v P") 'vc-push)
-(defun vc-push ()
-  "Do a git push"
-  (interactive)
-  (async-shell-command "git push"))
+; If not already in keymap and/or bound, add ability to "git push" to
+; keymap, bound to 'vc-push, and to "Tools | Version Control" menu.
+(if (not (fboundp 'vc-push))
+    (defun vc-push ()
+      "Do a git push"
+      (interactive)
+      (async-shell-command "git push")))
+(when (not (fboundp (global-key-binding (kbd "C-x v P"))))
+  (global-set-key (kbd "C-x v P") 'vc-push)
+  (define-key-after
+    (lookup-key global-map [menu-bar tools vc])
+    [vc-push]
+    '("Push to upstream" . vc-push)
+    'vc-revert))
 
 (put 'narrow-to-region 'disabled nil)
 
