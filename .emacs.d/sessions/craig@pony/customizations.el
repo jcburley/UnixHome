246bff3549b2b7dbccf1a96edb7a41ab60fc2698
James Craig Burley <james-github@burleyarch.com>
2018-09-02 00:43:57 -0400

    More git aliases; new Emacs module added

---
+++ .emacs.d/sessions/craig@pony/customizations.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (markdown-mode ## go-mode use-package rainbow-delimiters paredit exec-path-from-shell company clojure-mode-extra-font-locking cider))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
