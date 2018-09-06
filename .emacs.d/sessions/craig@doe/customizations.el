96041b66a1134ff258a89c77b04ef34e006ce0ad
James Craig Burley <james-github@burleyarch.com>
2018-09-06 15:53:31 -0400

    Added markdown-mode package to GNU Emacs on doe

--- .emacs.d/sessions/craig@doe/customizations.el
+++ .emacs.d/sessions/craig@doe/customizations.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (go-mode use-package rainbow-delimiters paredit company clojure-mode-extra-font-locking cider))))
+    (markdown-mode go-mode use-package rainbow-delimiters paredit company clojure-mode-extra-font-locking cider))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
