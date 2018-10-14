1df243c37f42ba6987ff8ef82627ab085e346b30
James Craig Burley <james-github@burleyarch.com>
2018-10-14 01:09:13 -0400

    Remove dead repo

--- components/emacs/craig/packages.el
+++ components/emacs/craig/packages.el
@@ -1,14 +1,14 @@
 (require 'package)
+(toggle-debug-on-error)
 (add-to-list 'package-archives
              '("marmalade" . "https://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives
-             '("tromey" . "http://tromey.com/elpa/") t)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("tromey" . "https://tromey.com/elpa/") t)
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
                     (not (gnutls-available-p))))
        (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
-  (add-to-list 'package-archives (cons "melpa" url) t))
+  (add-to-list 'package-archives
+               (cons "melpa" url) t))
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
