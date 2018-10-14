279e256206c10ae347efca158988ea52b87d6261
James Craig Burley <james-github@burleyarch.com>
2018-10-14 08:52:07 -0400

    Ensure packages are not enabled too early

--- components/emacs/init.el
+++ components/emacs/init.el
@@ -8,7 +8,8 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-;(package-initialize)
+;;(package-initialize)
+(setq package-enable-at-startup nil)
 
 (defvar username-at-systemname
   (concat (user-login-name) "@" (downcase (system-name)))
