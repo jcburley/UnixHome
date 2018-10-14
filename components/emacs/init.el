d9cb7d56d1663174250e63c90b06611828110031
James Craig Burley <james-github@burleyarch.com>
2018-10-14 00:38:51 -0400

    Bring new intermediary script into fold, renamed, and install

--- components/emacs/init.el
+++ components/emacs/init.el
@@ -35,7 +35,7 @@
 	    'custom-prompt-customize-unsaved-options))
 
 ;; I want M-! (shell-command) to pick up my aliases and so run .bashrc:
-(setq shell-file-name "bash-for-emacs.sh")  ; Does this: BASH_ENV="~/.bashrc" exec bash "$@"
+(setq shell-file-name "bash-for-emacs")  ; Does this: BASH_ENV="~/.bashrc" exec bash "$@"
 (setq shell-command-switch "-c")  ; GNU Emacs currently does not support multiple options.
 ;; (setq shell-command-switch (or (getenv "SHELL") "/bin/bash"))  ; GNU Emacs currently does not support multiple options.
 
