fa104305bb3398dade4857b8e4b1ca366b7b2d66
James Craig Burley <430319+jcburley@users.noreply.github.com>
2019-09-09 16:58:47 -0400

    Clean up turtle's Emacs config, enabling auto-revert mode

---
+++ components/emacs/sessions/craig@turtle/customizations.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(global-auto-revert-mode t)
+ '(package-selected-packages
+   (quote
+    (markdown-mode go-mode use-package company rainbow-delimiters cider clojure-mode-extra-font-locking clojure-mode paredit))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
