6fc3eb4a46c54661f7427035c18aa902f21626d2
James Craig Burley <430319+jcburley@users.noreply.github.com>
2021-01-26 17:39:15 -0500

    Updated due to recent changes

--- components/emacs/sessions/craig@turtle/customizations.el
+++ components/emacs/sessions/craig@turtle/customizations.el
@@ -3,10 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(global-auto-revert-mode t)
  '(package-selected-packages
-   (quote
-    (markdown-mode go-mode use-package company rainbow-delimiters cider clojure-mode-extra-font-locking clojure-mode paredit))))
+   '(companyx use-package rainbow-delimiters paredit markdown-mode go-mode company clojure-mode-extra-font-locking cider)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
