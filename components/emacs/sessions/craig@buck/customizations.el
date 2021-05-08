9404c3fd61a9d6c82974e3112df66b4dfb5faa60
James Craig Burley <430319+jcburley@users.noreply.github.com>
2021-05-08 10:59:27 -0400

    Update Windows customizations

--- components/emacs/sessions/craig@buck/customizations.el
+++ components/emacs/sessions/craig@buck/customizations.el
@@ -5,8 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(global-auto-revert-mode t)
  '(package-selected-packages
-   (quote
-    (go-mode org use-package company rainbow-delimiters cider clojure-mode-extra-font-locking clojure-mode paredit))))
+   '(go-mode org use-package company rainbow-delimiters cider clojure-mode-extra-font-locking clojure-mode paredit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
