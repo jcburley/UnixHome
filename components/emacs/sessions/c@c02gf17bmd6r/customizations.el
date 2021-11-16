f76e276332a233359f307152836938db57c2654c
James Craig Burley <james-github@burleyarch.com>
2021-11-16 12:37:27 -0500

    New work laptop

---
+++ components/emacs/sessions/c@c02gf17bmd6r/customizations.el
@@ -0,0 +1,21 @@
+(global-set-key (kbd "<M-up>") 'scroll-down-command)
+(global-set-key (kbd "<M-down>") 'scroll-up-command)
+(global-set-key (kbd "<s-up>") 'beginning-of-buffer)
+(global-set-key (kbd "<s-down>") 'end-of-buffer)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(compile-command "build ")
+ '(mac-command-modifier 'super)
+ '(mac-option-modifier 'meta)
+ '(package-selected-packages
+   '(use-package disable-mouse company rainbow-delimiters cider clojure-mode-extra-font-locking clojure-mode paredit exec-path-from-shell)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
