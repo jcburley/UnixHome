6ba503e60f60f092da0f0d8da860a2ce96721360
James Craig Burley <james-github@burleyarch.com>
2021-11-24 09:08:11 -0500

    Add go-mode to wolf and Namely laptops

--- components/emacs/sessions/craig@wolf/customizations.el
+++ components/emacs/sessions/craig@wolf/customizations.el
@@ -9,7 +9,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(use-package disable-mouse company rainbow-delimiters cider clojure-mode-extra-font-locking clojure-mode paredit exec-path-from-shell)))
+   '(use-package disable-mouse company rainbow-delimiters cider clojure-mode-extra-font-locking clojure-mode paredit exec-path-from-shell go-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
