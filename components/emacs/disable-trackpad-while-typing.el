b62a7bc77c7c2ac3b467d960d018081ed8e03230
James Craig Burley <430319+jcburley@users.noreply.github.com>
2022-07-27 05:06:26 -0400

    Fix mouse disabling while typing

--- components/emacs/disable-trackpad-while-typing.el
+++ components/emacs/disable-trackpad-while-typing.el
@@ -5,18 +5,18 @@
 (defun jogo3000/disable-mouse ()
   "Disable mouse."
   (setq jogo3000/mouse-disabled t)
-  (global-disable-mouse-mode t))
+  (global-disable-mouse-mode))
 
 (defun jogo3000/enable-mouse ()
   "Enable mouse."
-  (global-disable-mouse-mode -1)
+  (global-disable-mouse-mode t)
   (setq jogo3000/mouse-disabled nil))
 
 (defun jogo3000/temporarily-disable-mouse ()
   "Temporarily disable mouse."
   (unless jogo3000/mouse-disabled
     (jogo3000/disable-mouse)
-    (run-with-timer 2 nil 'jogo3000/enable-mouse)))
+    (run-with-timer 3 nil 'jogo3000/enable-mouse)))
 
 (defun jogo3000/disable-touchpad-mode ()
   "Make it so that touchpad is inactive when I type."
