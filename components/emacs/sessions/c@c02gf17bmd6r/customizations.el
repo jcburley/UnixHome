1c1d24d17d7917eba15d47e4db610c3dcd6517b5
James Craig Burley <430319+jcburley@users.noreply.github.com>
2022-09-16 10:42:11 -0400

    Add typescript-mode to Emacs config at work

--- components/emacs/sessions/c@c02gf17bmd6r/customizations.el
+++ components/emacs/sessions/c@c02gf17bmd6r/customizations.el
@@ -9,7 +9,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(trimspace-mode use-package rainbow-delimiters paredit markdown-mode go-mode exec-path-from-shell disable-mouse company clojure-mode-extra-font-locking cider)))
+   '(typescript-mode trimspace-mode use-package rainbow-delimiters paredit markdown-mode go-mode exec-path-from-shell disable-mouse company clojure-mode-extra-font-locking cider)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
